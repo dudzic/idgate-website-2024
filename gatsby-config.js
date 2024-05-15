0a4bc9f3ebb7d0f06b1846cb8ab79b107c0d38d8
Marcin Dudzic <marcin.dudzic@movecloser.pl>
2024-05-15 23:42:41 +0200

    Cleanup

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,16 @@ module.exports = {
   siteMetadata: {
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    'gatsby-plugin-postcss',
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `montserrat`,
+        ],
+        display: 'swap'
+      }
+    }
+  ],
 }
